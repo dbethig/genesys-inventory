460b68828c056c7949617fd536ebff49095deb3b
dbethig <60881384+dbethig@users.noreply.github.com>
2020-07-23 16:58:53 +0100

    Update header.php

--- app/views/themes/default/header.php
+++ app/views/themes/default/header.php
@@ -4,9 +4,15 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="msapplication-TileColor" content="#ffc40d">
+		<meta name="theme-color" content="#ffffff">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 		<script src="https://kit.fontawesome.com/e8dfc40b63.js" crossorigin="anonymous"></script>
-
 		<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <title><?php echo $pageName; ?></title>
   </head>
